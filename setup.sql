6ff5a541793fbfc5a48f7fea6a317728e9a59479
Eliska Ryklova <ryklovae@coventry.ac.uk>
2022-03-05 18:47:12 +0000

    DB table - games

--- setup.sql
+++ setup.sql
@@ -1,9 +1,16 @@
+-- mysql -u root -p
+-- p455w0rd
+-- SHOW DATABASES;
+-- use website;
+-- SHOW TABLES;
+-- source setup.sql
 
 -- make sure the websiteuser account is set up and has the correct privileges
 CREATE USER IF NOT EXISTS websiteuser IDENTIFIED BY 'websitepassword';
 GRANT INSERT, SELECT, UPDATE, DELETE ON website.* TO websiteuser;
 
 DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS games;
 
 CREATE TABLE IF NOT EXISTS accounts (
   id MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -13,3 +20,14 @@ CREATE TABLE IF NOT EXISTS accounts (
 
 INSERT INTO accounts(user, pass)
 	VALUES("doej", "$2b$10$gL33obKAFUT5DK3pEbh72OIHztsWBniBBh.PdeKOrF1yr5KFAsdZO");
+
+CREATE TABLE IF NOT EXISTS games (
+  id MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(60) NOT NULL,
+  publisher VARCHAR(60) NOT NULL,
+  year YEAR(4) NOT NULL,
+  description TEXT NOT NULL,
+  image BLOB DEFAULT NULL,
+  creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  username VARCHAR(25) NOT NULL
+);
