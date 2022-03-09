d72052671e62019498ed153a64495bf15f393852
Eliska Ryklova <ryklovae@coventry.ac.uk>
2022-03-09 18:05:03 +0000

    UAT for the game form - missing data, exceeding the maximum length of text input fields; Code linting - removed await from loops

--- uat/feature1.test.js
+++ uat/feature1.test.js
@@ -85,12 +85,68 @@ Deno.test('Adding a new game     ', async () => {
             const currentYear = date.getFullYear()
             const selectedYear = 1980 + Math.round((currentYear - 1980)/2)
             const clicks = selectedYear - 1981
-            for (let i = 0; i < clicks; i++) await page.keyboard.press('ArrowLeft')
+            let storedPromises = []
+            for (let i = 0; i < clicks; i++) storedPromises += page.keyboard.press('ArrowLeft')
+            await Promise.all(storedPromises)
     // AND I fill in "description" with "Space Invaders is a fixed shooter in which the player moves a laser cannon horizontally across the bottom of the screen and fires at aliens overhead."
             await page.type('textarea[name="description"]', 'Space Invaders is a fixed shooter in which the player moves a laser cannon horizontally across the bottom of the screen and fires at aliens overhead.')
     // AND I click the "Submit" button
             await page.click('input[type="submit"]', { waitUntil: 'networkidle0' })
     // THEN I should be redirected to the "Home" page
             await assertEquals(page.url(), url, 'not on the homepage')
+            await browser.close()
+})
+
+// SCENARIO: Submitting the game form with missing data
+Deno.test('Submitting the game form with missing data     ', async () => {
+    // GIVEN I am logged in
+            const browser = await puppeteer.launch({ headless: true })
+            //const browser = await puppeteer.launch({ headless: false, slowMo: 50  })
+            const page = await browser.newPage()
+            await page.goto(url + 'login', { waitUntil: 'networkidle0' })
+            await page.type('input[name="username"]', 'doej')
+            await page.type('input[name="password"]', 'p455w0rd')
+            await page.click('input[type="submit"]', { waitUntil: 'networkidle0' })
+    // AND I am on the "Add Game" form page
+            await page.goto(url + 'add-game', { waitUntil: 'networkidle0' })
+    // WHEN I fill in "name" with "Space Invaders"
+            await page.type('input[name="name"]', 'Space Invaders')
+    // AND I select "1981" from "year"
+            await page.keyboard.press('Tab')
+            await page.keyboard.press('Tab')
+            const date = new Date()
+            const currentYear = date.getFullYear()
+            const selectedYear = 1980 + Math.round((currentYear - 1980)/2)
+            const clicks = selectedYear - 1981
+            let storedPromises = []
+            for (let i = 0; i < clicks; i++) storedPromises += page.keyboard.press('ArrowLeft')
+            await Promise.all(storedPromises)
+    // AND I fill in "description" with "Space Invaders is a fixed shooter."
+            await page.type('textarea[name="description"]', 'Space Invaders is a fixed shooter.')
+    // AND I click the "Submit" button
+            await page.click('input[type="submit"]', { waitUntil: 'networkidle0' })
+    // THEN I should be redirected to the "Home" page
+            await assertEquals(page.url(), url + "add-game", 'not on the form page')
+            await browser.close()
+})
+
+// SCENARIO: Exceeding the maximum length of 60 in an input field
+Deno.test('Exceeding the maximum length of 60 in an input field     ', async () => {
+    // GIVEN I am logged in
+            const browser = await puppeteer.launch({ headless: true })
+            //const browser = await puppeteer.launch({ headless: false, slowMo: 10  })
+            const page = await browser.newPage()
+            await page.goto(url + 'login', { waitUntil: 'networkidle0' })
+            await page.type('input[name="username"]', 'doej')
+            await page.type('input[name="password"]', 'p455w0rd')
+            await page.click('input[type="submit"]', { waitUntil: 'networkidle0' })
+    // AND I am on the "Add Game" form page
+            await page.goto(url + 'add-game', { waitUntil: 'networkidle0' })
+    // WHEN I fill in "name" with "a" 61 times
+            const desiredName = 'a'.repeat(61)
+            await page.type('input[name="name"]', desiredName)
+    // THEN I should see "a" 60 times in the "name" field
+            const actualName = await page.$eval('input[name="name"]', node => node.value)
+            await assertEquals(actualName.length, 60, 'name is not 60 characters long')
             await browser.close()
 })
