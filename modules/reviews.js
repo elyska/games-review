c5ce71a0e0b164645d047fcb059c15b054a32862
Eliska Ryklova <ryklovae@coventry.ac.uk>
2022-03-12 18:14:11 +0000

    Unit testing for addReview function

---
+++ modules/reviews.js
@@ -0,0 +1,21 @@
+/* reviews.js */
+
+/**
+ * Games Module.
+ * @module reviews
+ */
+
+import { db } from "database"
+import Ajv from '../ajv.js'
+
+/**
+ * Saves a review in the database
+ * @param {Object} data
+ * @param {string} username
+ * @returns {boolean}
+ * @throws Will throw an error if the data validation fails.
+ */
+export async function addReview(data, username) {
+    
+    return true
+}
---
+++ tests/reviews.test.js
@@ -0,0 +1,45 @@
+
+/* reviews.test.js */
+
+import { assertEquals, assert, assertExists, fail } from 'https://deno.land/std@0.79.0/testing/asserts.ts'
+
+import { addReview } from '../modules/reviews.js'
+
+Deno.test({
+  name: "add new review",
+  async fn(test) {
+    
+    await test.step("add a valid review", async () => {
+        const data = {
+            rating: 4,
+            review: "Good game!",
+            username: "user1",
+            creationDate: new Date('2022-03-12T18:08:11.000Z')
+        }
+        const username = "doej"
+        const returned = await addReview(data, username)
+        assert(returned, "failed to add a new review")
+    })
+
+    await test.step("add an ivalid review", async () => {
+        const data = {
+            rating: "4",
+            review: "Good game!",
+            username: "user1",
+            creationDate: new Date('2022-03-12T18:08:11.000Z')
+        }
+        const username = "doej"
+
+        try {
+            await addReview(data, username)
+            fail("failed to throw an exception")
+        }
+        catch (err) {
+            assertEquals(err.message, "invalid review data")
+        }
+    })
+
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
