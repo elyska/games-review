31b5847989b75be0e0229077e666759ef5c5786c
Eliska Ryklova <ryklovae@coventry.ac.uk>
2022-03-15 16:46:25 +0000

    Git hooks

--- modules/accounts.js
+++ modules/accounts.js
@@ -44,7 +44,7 @@ export async function login(data) {
  * @returns {boolean}
  */
 export async function register(data) {
-	let password = await hash(data.password, salt)
+	const password = await hash(data.password, salt)
 	const sql = `INSERT INTO accounts(user, pass) VALUES("${data.username}", "${password}")`
 	console.log(sql)
 	await db.query(sql)
